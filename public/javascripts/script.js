d5cb0717a37ae6a48689e7b09400cbd490ca7a86
Roxanne <roxannebrod@yahoo.com>
2020-09-01 08:04:39 +0200

    incomplete lab

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,11 +6,30 @@ for (let i = 0; i < mashPotatoes.length; i++) {
   console.log(mashPotatoes[i])
 }
 
+const addImage = img => document.querySelector('#table').innerHTML +=
+  ` <img src = "public/images/${img}.jpg" alt = "${img}"/>`
+
+
 // Iteration 1 using callbacks
 addFood(steak[0], '#steak', () => {
   // ... your code here
   addFood(steak[1], '#steak', () => {
-
+    addFood(steak[2], '#steak', () => {
+      addFood(steak[3], '#steak', () => {
+        addFood(steak[4], '#steak', () => {
+          addFood(steak[5], '#steak', () => {
+            addFood(steak[6], '#steak', () => {
+              addFood(steak[7], '#steak', () => {
+                // document.querySelector('#table').innerHTML += 
+                // '<img src = "public/images/steak.jpg" alt = "steak"/>'
+                // addImage('steak');
+                console.log('done');
+              })
+            })
+          })
+        })
+      })
+    })
   })
 });
 
@@ -18,14 +37,38 @@ addFood(steak[0], '#steak', () => {
 
 // Iteration 2 using `.then()`
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
+  addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
+    addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
+      addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
+        addFood(mashPotatoes[4], '#mashPotatoes').then(() => {
+          // document.querySelector('#table').innerHTML += 
+          //  '<img src = "public/images/mashPotatoes.jpg" alt = "potatoes"/>'
+          //addImage('mashPotatoes');
+        })
+      })
+    })
+  })
 });
 
 // Iteration 3 using async and await
 
-  async function makeFood(step) {
-    // ... your code here
-    
+async function makeFood(steps, id) {
+  for (let step of steps) {
+    await addFood(step, id)
   }
-  makeFood(eachStep);
+  //document.querySelector('#table').innerHTML += 
+  // '<img src = "public/images/brusselSprouts.jpg" alt = "brusselSprouts"/>'
+  addImage(id.slice(1));
+}
+//makeFood(brusselSprouts, '#brusselSprouts');
+
+
+Promise.all([
+  makeFood(steak, '#steak'),
+  makeFood(brusselSprouts, '#brusselSprouts'),
+  makeFood(mashPotatoes, '#mashPotatoes'),
+]).then(response => {
+  // add the button
+  document.body.innerHTML += `<button 
+  onclick ="new Audio('public/media/dinnerIsServed.mp3').play()">Dinner is served</button>`;
+});
